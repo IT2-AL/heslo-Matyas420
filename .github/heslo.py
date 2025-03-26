2500435ad25330fd4b54e1cf3ef51ed64fbe3ea0
Matyas420 <98987@student.vitkovickastredni.cz>
2025-03-26 11:34:16 +0100

    heslo_skoro

--- .github/heslo.py
+++ .github/heslo.py
@@ -1,6 +1,9 @@
 def overeni_hesla(heslo):
+    if len(heslo) < 8:
+        print("Heslo je příliš krátké")
     if len(heslo) >= 8:
-        print("Heslo je dostatečně silné")
+        print("Heslo je dostatečné")
+    
     elif heslo.isalnum():
         return "Heslo musí obsahovat alespoň jeden znak"
     elif heslo.islower() or heslo.isupper():
@@ -9,9 +12,16 @@ def overeni_hesla(heslo):
         return "Heslo musí obsahovat číslo."
     else:
         print("Heslo je nedostatečně silné, jednoduše řečeno skill issue")
+        
+def kontrola_hesla(heslo, heslo2):
+    if heslo2 == heslo:
+        print("Hesla jsou totožné")
+    else:
+        print("Hesla se neshodují")
     
-    return "Tvoje heslo je v pořádku"
 
 heslo = input("Zadejte Heslo, které bude mít více než 8 znaků:")
-print(overeni_hesla(heslo))
+overeni_hesla(heslo)
 
+heslo2 = input("Znovu zadejte své heslo pro kontrolu, jestli se hesla zhodují:")
+kontrola_hesla(heslo, heslo2)

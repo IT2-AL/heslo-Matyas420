97345d4f266ff905a3263177ed07269099e19e7d
Matyas420 <98987@student.vitkovickastredni.cz>
2025-03-26 16:37:48 +0100

    heslo_bez_komentar

--- .github/heslo.py
+++ .github/heslo.py
@@ -1,27 +1,36 @@
 def overeni_hesla(heslo):
     if len(heslo) < 8:
-        print("Heslo je příliš krátké")
-    if len(heslo) >= 8:
-        print("Heslo je dostatečné")
+        print("Heslo je příliš krátké (minimálně 8 znaků)")
+        return False
     
-    elif heslo.isalnum():
-        return "Heslo musí obsahovat alespoň jeden znak"
-    elif heslo.islower() or heslo.isupper():
-        return "Heslo musí obsahovat velké i malé písmeno."
-    elif not any(H.isdigit() for H in heslo):
-        return "Heslo musí obsahovat číslo."
-    else:
-        print("Heslo je nedostatečně silné, jednoduše řečeno skill issue")
-        
+    if not any(h.isdigit() for h in heslo):
+        print("Heslo musí obsahovat alespoň jedno číslo.")
+        return False
+    
+    if not any(h.isalpha() for h in heslo):
+        print("Heslo musí obsahovat alespoň jedno písmeno.")
+        return False
+    
+    if heslo.islower():
+        print("Heslo musí obsahovat alespoň jedno velké písmeno.")
+        return False
+    
+    if heslo.isupper():
+        print("Heslo musí obsahovat alespoň jedno malé písmeno.")
+        return False
+    
+    print("Heslo je dostatečně silné.")
+    return True
+
 def kontrola_hesla(heslo, heslo2):
     if heslo2 == heslo:
-        print("Hesla jsou totožné")
+        print("Hesla jsou totožná.")
     else:
-        print("Hesla se neshodují")
-    
+        print("Hesla se neshodují.")
 
-heslo = input("Zadejte Heslo, které bude mít více než 8 znaků:")
-overeni_hesla(heslo)
+heslo = input("Zadejte heslo, které bude mít více než 8 znaků: ")
 
-heslo2 = input("Znovu zadejte své heslo pro kontrolu, jestli se hesla zhodují:")
+while not overeni_hesla(heslo):
+    heslo = input("Zadejte heslo, které bude mít více než 8 znaků: ")
+heslo2 = input("Znovu zadejte své heslo pro kontrolu, jestli se hesla zhodují: ")
 kontrola_hesla(heslo, heslo2)
